cabd3c98869744c2a80bbc279a94cec139ec5554
Dawid <dawidn392@gmail.com>
2023-12-10 15:06:01 +0100

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Dawid Nalepa on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
